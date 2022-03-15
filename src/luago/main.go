74577488fc773c8b85a0b811404759d62f1975c8
834930269 <834930269@qq.com>
2022-03-15 15:30:54 +0800

    [feature] chunk流中读取opcode

--- src/luago/main.go
+++ src/luago/main.go
@@ -4,6 +4,7 @@ import "fmt"
 import "io/ioutil"
 import "os"
 import "luago/binchunk"
+import . "luago/vm"
 
 //简化版的模拟读取chunk
 func main(){
@@ -48,7 +49,11 @@ func printCode(f *binchunk.Prototype){
 		if len(f.LineInfo) > 0 {
 			line = fmt.Sprintf("%d",f.LineInfo[pc])
 		}
-		fmt.Printf("\t%d\t[%s]\t0x%08X\n",pc+1,line,c)
+
+		i := Instruction(c)
+		fmt.Printf("\t%d\t[%s]\t0x%08X\t%s\t",pc+1,line,c,i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
 	}
 }
 
@@ -87,4 +92,42 @@ func upvalName(f *binchunk.Prototype,idx int) string {
 		return f.UpvalueNames[idx]
 	}
 	return "-"
+}
+
+func printOperands(i Instruction) {
+	switch i.OpMode() {
+	case IABC:
+		a, b, c := i.ABC()
+
+		fmt.Printf("%d", a)
+		if i.BMode() != OpArgN {
+			if b > 0xFF {
+				fmt.Printf(" %d", -1-b&0xFF)
+			} else {
+				fmt.Printf(" %d", b)
+			}
+		}
+		if i.CMode() != OpArgN {
+			if c > 0xFF {
+				fmt.Printf(" %d", -1-c&0xFF)
+			} else {
+				fmt.Printf(" %d", c)
+			}
+		}
+	case IABx:
+		a, bx := i.ABx()
+
+		fmt.Printf("%d", a)
+		if i.BMode() == OpArgK {
+			fmt.Printf(" %d", -1-bx)
+		} else if i.BMode() == OpArgU {
+			fmt.Printf(" %d", bx)
+		}
+	case IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case IAx:
+		ax := i.Ax()
+		fmt.Printf("%d", -1-ax)
+	}
 }
